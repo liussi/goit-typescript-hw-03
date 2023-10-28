fc0f2567a272cdcfb1bca55567898af12c6c8bb1
liussi <125599487+liussi@users.noreply.github.com>
2023-10-28 10:02:48 +0300

    fix

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,54 @@
+
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  door: boolean; 
+  key: Key; 
+  tenants: Person[] = []; 
+
+  constructor(key: Key) {
+    this.door = false; 
+    this.key = key;
+  }
+
+  abstract openDoor(key: Key): void;
+
+  comeIn(person: Person) {
+    if (this.door) {
+      this.tenants.push(person);
+      } 
+  }
+}
+
+class MyHouse extends House {
+    openDoor(key: Key): void{
+        if (key.getSignature() === this.key.getSignature()) {
+          this.door = true;
+      }
+  }
+}
 const key = new Key();
 
 const house = new MyHouse(key);
